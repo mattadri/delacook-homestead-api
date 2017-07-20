c52b865b893057bdf3eef73ee272287874437d73
adrifolio <adrifolio@users.noreply.github.com>
2017-07-20 17:58:06 -0500

    created new schema for plant lineages

--- app/models/plant.py
+++ app/models/plant.py
@@ -66,3 +66,166 @@ class LeafCycle(db.Model):
     modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
     enum = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
+
+
+class PlantNotes(db.Model):
+    __tablename__ = 'plantnotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_cloning_fk = db.Column(db.Integer, db.ForeignKey('plants.id'))
+    plant_cloning = db.relationship('Plant', backref=db.backref('plantnotes'))
+    note_date = db.Column(db.DateTime)
+    note = db.Column(db.Text)
+
+
+class PlantLineage(db.Model):
+    __tablename__ = 'plantlineages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_fk = db.Column(db.Integer, db.ForeignKey('plants.id'))
+    plant = db.relationship('Plant', backref=db.backref('plantlineages'))
+    plant_generation_fk = db.Column(db.Integer, db.ForeignKey('plantgenerations.id'))
+    plant_generation = db.relationship('PlantGeneration', backref=db.backref('plantlineages'))
+    previous_plant_generation_fk = db.Column(db.Integer, db.ForeignKey('plantgenerations.id'))
+    previous_plant_generation = db.relationship('PlantGeneration', backref=db.backref('plantlineages'))
+    is_origin = db.Column(db.Boolean, default=False)
+    is_dead = db.Column(db.Boolean, default=False)
+
+
+class PlantGeneration(db.Model):
+    __tablename__ = 'plantgenerations'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    date_seeds_collected = db.Column(db.DateTime)
+
+
+# A planting is an object containing data about the planting of a single generation. One generation
+# could have many plantings.
+class Planting(db.Model):
+    __tablename__ = 'plantings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_generation_fk = db.Column(db.Integer, db.ForeignKey('plantgenerations.id'))
+    plant_generation = db.relationship('PlantGeneration', backref=db.backref('plantings'))
+    planting_physical_source_fk = db.Column(db.Integer, db.ForeignKey('plantingphysicalsources.id'))
+    planting_physical_source = db.relationship('PlantingPhysicalSource', backref=db.backref('plantings'))
+    seeds_started = db.Column(db.Integer)
+    date_seeds_started = db.Column(db.DateTime)
+    starting_medium = db.Column(db.String)
+    seeds_sprouted = db.Column(db.Integer)
+    date_first_seeds_sprouted = db.Column(db.DateTime)
+    sprouts_planted = db.Column(db.Integer)
+    date_sprouts_planted = db.Column(db.DateTime)
+    plants_survived = db.Column(db.Integer)
+
+
+class PlantingNotes(db.Model):
+    __tablename__ = 'plantingnotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_cloning_fk = db.Column(db.Integer, db.ForeignKey('plantings.id'))
+    plant_cloning = db.relationship('Planting', backref=db.backref('plantingnotes'))
+    note_date = db.Column(db.DateTime)
+    note = db.Column(db.Text)
+
+
+# CUTTING, SEED, ROOT, BULB, UNKNOWN
+class PlantingPhysicalSource(db.Model):
+    __tablename__ = 'plantingphysicalsources'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    enum = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+
+# SOURCE (EX. BLOOMERS NURSERY, RENEE'S GARDEN, SEED SAVERS EXCHANGE)
+class PlantingSource(db.Model):
+    __tablename__ = 'plantingsources'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    name = db.Column(db.String, nullable=False)
+    planting_fk = db.Column(db.Integer, db.ForeignKey('plantings.id'))
+    planting = db.relationship('Planting', backref=db.backref('plantingsources'))
+
+
+class PlantClone(db.Model):
+    __tablename__ = 'plantclones'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_generation_fk = db.Column(db.Integer, db.ForeignKey('plantgenerations.id'))
+    plant_generation = db.relationship('PlantGeneration', backref=db.backref('plantclones'))
+    planting_fk = db.Column(db.Integer, db.ForeignKey('plantings.id'))
+    planting = db.relationship('Planting', backref=db.backref('plantclones'))
+
+
+class PlantCloning(db.Model):
+    __tablename__ = 'plantclonings'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_clone_fk = db.Column(db.Integer, db.ForeignKey('plantclones.id'))
+    plant_clone = db.relationship('PlantClone', backref=db.backref('plantclonings'))
+    date_cloned = db.Column(db.DateTime)
+    amount_cloned = db.Column(db.Integer)
+    amount_rooted = db.Column(db.Integer)
+    amount_planted = db.Column(db.Integer)
+    root_hormone_used = db.Column(db.Boolean)
+    rooting_medium = db.Column(db.String)
+
+
+class PlantCloningNotes(db.Model):
+    __tablename__ = 'plantcloningnotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_cloning_fk = db.Column(db.Integer, db.ForeignKey('plantclonings.id'))
+    plant_cloning = db.relationship('PlantCloning', backref=db.backref('plantcloningnotes'))
+    note_date = db.Column(db.DateTime)
+    note = db.Column(db.Text)
+
+
+class PlantHarvest(db.Model):
+    __tablename__ = 'plantharvests'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_generation_fk = db.Column(db.Integer, db.ForeignKey('plantgenerations.id'))
+    plant_generation = db.relationship('PlantGeneration', backref=db.backref('plantharvests'))
+    start_date = db.Column(db.DateTime)
+    finish_date = db.Column(db.DateTime)
+    grams_harvested = db.Column(db.Integer)
+
+
+class PlantHarvestNotes(db.Model):
+    __tablename__ = 'plantharvestnotes'
+
+    id = db.Column(db.Integer, primary_key=True)
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    plant_harvest_fk = db.Column(db.Integer, db.ForeignKey('plantharvests.id'))
+    plant_harvest = db.relationship('PlantHarvest', backref=db.backref('plantharvestnotes'))
+    created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    modified = db.Column(db.DateTime, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    note_date = db.Column(db.DateTime)
+    note = db.Column(db.Text)
+
+
